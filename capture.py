4704c03b3bd745417f576d610b38d7cef9645238
Julian WEBER <julian.weber@hotmail.fr>
2020-02-29 23:31:21 +0100

    Working dataset generator

--- capture.py
+++ capture.py
@@ -1,8 +1,11 @@
 from pynput import keyboard
 import time
 import pandas as pd 
+import sounddevice as sd
+from scipy.io.wavfile import write
 
 duration = 5
+fs = 48000
 events = []
 
 def on_press(key):
@@ -14,11 +17,19 @@ def on_press(key):
     events.append({'key': res, 'timestamp': time.time_ns()})
     print("key: "+res+" timestamp: "+str(time.time_ns()))
     
-
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
-time.sleep(duration)
-print(events)
+print("Starting to listen for "+str(duration)+" seconds...")
+sound = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+sd.wait()
+
+listener.stop()
+print("Playback...")
+sd.play(sound, fs)
+sd.wait()
+
+print("Writing output...")
 data = pd.DataFrame(events)
-data.to_csv('')
+data.to_csv('dataset/timestamp.csv')
+write('dataset/output.wav', fs, sound)
