d07bc04547dca7e611d94e08cb9f2d26a79d216d
Julian WEBER <julian.weber@hotmail.fr>
2020-03-05 08:36:13 +0100

    working model

--- generate.py
+++ generate.py
@@ -15,13 +15,13 @@ def generateImage(row):
     line = row['timestamp']
     sample = signalData[ms2steps(line - delay):ms2steps(line - delay)+ms2steps(duration)]
     fig = plt.figure(frameon=False)
-    fig.set_size_inches(10,10)
+    fig.set_size_inches(2,2)
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     fig.add_axes(ax)
     plt.specgram(sample,Fs=samplingFrequency,cmap='gray')
     image_name = str(int(row['timestamp']))+'.png'
-    plt.savefig(path+image_name, dpi=10)
+    plt.savefig(path+image_name, dpi=64)
     return image_name
 
 data['timestamp'] = data.apply(lambda row: generateImage(row), axis=1)
--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 import torch
 import pandas as pd
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from skimage import io, transform
 
 path = 'dataset/'
@@ -27,6 +27,83 @@ class AudioKeyLoggerDataset(Dataset):
         img_name = os.path.join(self.image_dir, self.timestamps.iloc[idx, 2])
         image = io.imread(img_name, as_gray=True)
         key = self.timestamps.iloc[idx, 1]
-        return { 'image': image, 'key': self.labels.get(key)}
+        return { 'image': torch.tensor(image).unsqueeze(0), 'key': self.labels.get(key)}
+    
+    def getNumKey(self):
+        return len(self.classes)
+
+class ClassifierCNN(nn.Module):
+    def __init__(self, num_key):
+        super(ClassifierCNN, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 16, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(16),
+            nn.MaxPool2d(2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2)
+        )
+        self.dense1 = nn.Sequential(
+            nn.Linear(64*16*16, 32*16*16),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(8192)
+        )
+        self.dense2 = nn.Sequential(
+            nn.Linear(32*16*16, num_key)
+        )
+    
+    def forward(self, image):
+        x = self.conv1(image)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = x.view(1,-1)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        return x
 
 dataset = AudioKeyLoggerDataset(os.path.join(path, 'timestamps.csv'), os.path.join(path, 'images'))
+
+lengths = [int(len(dataset)*0.8), len(dataset)-int(len(dataset)*0.8)]
+train_set, test_set = torch.utils.data.random_split(dataset, lengths)
+testloader = torch.utils.data.DataLoader(train_set, batch_size=16, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(test_set, batch_size=16, shuffle=True, num_workers=2)
+
+model = ClassifierCNN(num_key=dataset.getNumKey())
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam()
+
+for epoch in range(5):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
